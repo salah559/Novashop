91a273f1d5871a0f62291ad62823a6544f52a3e5
salah559 <142224356+salah559@users.noreply.github.com>
2025-10-02 00:06:42 +0100

    Update placeholder-images.ts

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -1,1 +1,13 @@
-// Placeholder for placeholder-images.ts
+/*
+ * File: src/lib/placeholder-images.ts
+ */
+import data from './placeholder-images.json';
+
+export type ImagePlaceholder = {
+  id: string;
+  description: string;
+  imageUrl: string;
+  imageHint: string;
+};
+
+export const PlaceHolderImages: ImagePlaceholder[] = data.placeholderImages;// Placeholder for placeholder-images.ts
